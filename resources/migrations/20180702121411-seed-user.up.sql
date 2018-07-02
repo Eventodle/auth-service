dfc31ff18116863b03ede522476255ffe9c752f2
Nicola <sheldr@gmail.com>
2018-07-02 19:58:48 +0200

    Add db seeds for user

---
+++ resources/migrations/20180702121411-seed-user.up.sql
@@ -0,0 +1,2 @@
+INSERT INTO users (id, first_name, last_name, email, pass)
+VALUES ('first', 'rod', 'rigo', 'foo@bar.com', 'bcrypt+sha512$1a2d86cf73da7d22f30c55e2b548288c$12$d12ed25fc323bd02533dde0799238d0711c29ebe8cfd71c2');
